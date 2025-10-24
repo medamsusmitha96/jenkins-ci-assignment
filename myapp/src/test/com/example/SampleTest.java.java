65580315f5b69d4b9ed12682d17a3900dbd7f05c
MedamSusmitha <medamsusmitha96@gmail.com>
2025-10-24 15:35:34 +0530

    fileupdate

---
+++ myapp/src/test/com/example/SampleTest.java.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+    @Test
+    public void testAlwaysPasses() {
+        assertTrue(true);
+    }
+}
